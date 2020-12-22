d0253322dcdeaad3e31ac95bcccadc3bee58584d
Ilya Gaivoronskiy <gaivor1@gmail.com>
2020-12-22 16:03:39 +0300

    interface features

--- TableViewApp/TableViewApp/ViewController.swift
+++ TableViewApp/TableViewApp/ViewController.swift
@@ -7,24 +7,42 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let names = [
+        "Шаурмечная", "Кружка", "Макдак",
+        "Второе дыхание", "Шашлычная №1"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - table view data source
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return names.count
     } // число строк необходимое для работы с таблицей
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = "Cell"
+        cell?.textLabel?.text = names[indexPath.row]
+        cell?.imageView?.image = UIImage(named: names[indexPath.row])
+        cell?.imageView?.layer.cornerRadius = cell!.frame.size.height / 2
+        cell?.imageView?.clipsToBounds = true
         return cell!
     } // метод для работы с контентом ячейки
     
+// MARK: - Table view delegate
     
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
+
+
+    
 
 
 }
